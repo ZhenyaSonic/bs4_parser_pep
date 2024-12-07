7993b3cb6e4418625c2ae7edfe2bfdb8373b632a
Евгений Братанов <sonik.zhenyaa@gmail.com>
2024-12-07 11:30:02 +0300

    configs

--- src/constants.py
+++ src/constants.py
@@ -4,6 +4,11 @@ MAIN_DOC_URL = 'https://docs.python.org/3/'
 PEP_URL = 'https://peps.python.org/'
 BASE_DIR = Path(__file__).parent
 DATETIME_FORMAT = '%Y-%m-%d_%H-%M-%S'
+FORMAT_PRETTY = 'pretty'
+FORMAT_FILE = 'file'
+LOG_DIR = BASE_DIR / 'logs'
+LOG_FILE = LOG_DIR / 'parser.log'
+LOG_FORMAT = '"%(asctime)s - [%(levelname)s] - %(message)s"'
 EXPECTED_STATUS = {
     'A': ('Active', 'Accepted'),
     'D': ('Deferred',),
